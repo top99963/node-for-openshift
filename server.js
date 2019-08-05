28fd09d76e87caca776000134e6feb430102558f
Top <nonpawit.s@plaping.com>
2019-08-05 11:01:34 +0700

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 const requestHandler = (request, response) => {
   console.log(request.url)
