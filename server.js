ae6b0964d7c8c6053b432352a0f38743583d527c
Top <nonpawit.s@plaping.com>
2019-08-05 11:09:13 +0700

    sx

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello Node.js Server fuck!')
+  response.end('Hello Node.js Server fuck k!')
 }
 
 const server = http.createServer(requestHandler)
